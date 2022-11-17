6fb6876649d3c8998847bdfa9f55975242f78876
adityasharma004 <adityasharma3520@gmail.com>
2022-11-17 20:38:20 +0530

    c

---
+++ ram.py
@@ -0,0 +1,5 @@
+o =1
+while o == 1:
+    print("JAI SHREE RAAM")
+    break
+    
---
+++ while1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{ 
+int arr[]={12,23,34,56};
+// arr[2]=67;
+printf("%d\n",arr[0]);
+printf("%d\n",arr[1]);
+printf("%d\n",arr[2]);
+printf("%d\n",arr[3]);
+}
